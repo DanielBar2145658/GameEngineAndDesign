df89f79b69fca4a342ae1288cc20607daa07aca2
Daniel Bar <98904765+DanielBar2145658@users.noreply.github.com>
2024-10-08 09:53:47 -0400

    Build

--- Assets/Scripts/Observer/Hud.cs
+++ Assets/Scripts/Observer/Hud.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Hud : MonoBehaviour
+public class Hud : Observer
 {
+    public TextMesh score;
+
+
+
+
+    public override void Notify(Subject subject)
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Observer/Subject.cs
+++ Assets/Scripts/Observer/Subject.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public abstract class Subject
 {
-    
+    private readonly ArrayList _observers = new ArrayList();
+
+    protected void Attach(Observer observer) 
+    {
+        _observers.Add(observer);
+    }
+    protected void Detach(Observer observer)
+    {
+        _observers.Remove(observer);
+    }
+    protected void Notify()
+    {
+        foreach (Observer observer in _observers) 
+        {
+            observer.Notify(this);
+        }
+    }
+
 }
